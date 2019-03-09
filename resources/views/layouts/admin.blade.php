36282ffab3074659f029513ad34d5881e52a3444
ibaiway <ibaialberdi@gmail.com>
2019-03-10 00:50:15 +0200

    Datatables added to countries view

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,6 +24,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
         apply the skin class to the body tag so the changes take effect. -->
   <link rel="stylesheet" href="{{asset('adminlte/template/css/skins/skin-blue.min.css')}}">
 
+  @yield('css')
+  @yield('scripttop')
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -406,7 +408,7 @@ desired effect
 <script src="{{asset('adminlte/components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('adminlte/template/js/adminlte.min.js')}}"></script>
-
+@yield('script')
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
      user experience. -->
